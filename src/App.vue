f5d9c0c037b0604010d812f667894cefa1355c69
Jan Eberlein <code@janeberlein.de>
2024-02-01 21:49:38 +0100

    setup project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div>
+      <HelloWorld>
+        Test
+      </HelloWorld>
+    </div>
+  </header>
+
+  <main>
+    <HelloWorld>
+      Main
+    </HelloWorld>
+  </main>
+</template>
