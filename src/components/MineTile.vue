53dfb07b68c4cb268c088242e47b732c66a77bb3
Jan Eberlein <code@janeberlein.de>
2024-02-06 13:53:52 +0100

    refactors MineTile to be more readable

--- src/components/MineTile.vue
+++ src/components/MineTile.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { cells } from '@/store'
+import { computed } from 'vue';
 
 const props = defineProps({
   row: {
@@ -12,42 +13,45 @@ const props = defineProps({
   }
 })
 
+
+const cell = computed(() => {return cells.data[props.row][props.column]})
+
 var obstaclesNearby = 0;
 function getSurroundingCells(row, column) {
 
 }
 
 function revealCell(row, column) {
-  cells.data[props.row][props.column].revealed = true
+  cell.value.revealed = true
 }
 
-function rightClick(event) {
-  event.preventDefault()
-  switch (cells.data[props.row][props.column].marked) {
+function markCell() {
+  switch (cell.value.marked) {
     case '🚩':
-      cells.data[props.row][props.column].marked = '❓'
+      cell.value.marked = '❓'
       break;
     case '':
-      cells.data[props.row][props.column].marked = '🚩'
+      cell.value.marked = '🚩'
       break;
     default:
-    cells.data[props.row][props.column].marked = ''
+      cell.value.marked = ''
       break;
   }
 }
+
 </script>
 
 <template>
   <div
     class="flex items-center justify-center m-1 h-8 w-8 p-0 text-balance bg-slate-500 shadow-md shadow-slate-800 hover:m-0 hover:h-10 hover:w-10 hover:text-lg"
     @click="revealCell"
-    @click.right="rightClick($event)"
+    @click.right.prevent="markCell"
   >
-    <div v-if="cells.data[props.row][props.column].revealed">
-      {{ cells.data[props.row][props.column].obstacle }}
+    <div v-if="cell.revealed">
+      {{ cell.obstacle }}
     </div>
     <div v-else>
-      {{ cells.data[props.row][props.column].marked }}
+      {{ cell.marked }}
     </div>
   </div>
 </template>
